55bb185b6d48a0e8413045d74f6949c0468465a2
tsichun <tsichunwang@gmail.com>
2025-12-29 22:04:34 +0800

    feat: publish prophy

--- prophy/static/js/index.js
+++ prophy/static/js/index.js
@@ -38,25 +38,20 @@ $(document).ready(function() {
 			autoplaySpeed: 3000,
     }
 
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
+		// Initialize results carousel
+    bulmaCarousel.attach('.results-carousel', options);
 
-    // Loop on each carousel initialized
-    for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
-    	carousels[i].on('before:show', state => {
-    		console.log(state);
-    	});
+    // Initialize comparison and method carousels
+    var comparisonOptions = {
+			slidesToScroll: 1,
+			slidesToShow: 1,
+			loop: true,
+			infinite: true,
+			autoplay: false,
+			autoplaySpeed: 3000,
     }
-
-    // Access to bulmaCarousel instance of an element
-    var element = document.querySelector('#my-element');
-    if (element && element.bulmaCarousel) {
-    	// bulmaCarousel instance is available as element.bulmaCarousel
-    	element.bulmaCarousel.on('before-show', function(state) {
-    		console.log(state);
-    	});
-    }
+    bulmaCarousel.attach('.comparison-carousel', comparisonOptions);
+    bulmaCarousel.attach('.method-carousel', comparisonOptions);
 
     /*var player = document.getElementById('interpolation-video');
     player.addEventListener('loadedmetadata', function() {
